f837978972e84322e486af1c0e6255af1d88a4a0
Nithish Reddy <nithishreddyalampally@gmail.com>
2026-06-27 22:56:08 +0530

    initial commit

---
+++ sales_prediction.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load dataset
+df = pd.read_csv("Advertising.csv")
+
+# Display data
+print(df.head())
+
+# Check dataset information
+print(df.info())
+
+# Check missing values
+print(df.isnull().sum())
+
+# Visualization
+sns.pairplot(df)
+plt.show()
+
+# Features and target
+X = df[['TV', 'Radio', 'Newspaper']]
+y = df['Sales']
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Train model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Prediction
+y_pred = model.predict(X_test)
+
+# Evaluation
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Mean Squared Error:", mse)
+print("R2 Score:", r2)
+
+# Actual vs Predicted
+plt.figure(figsize=(8,5))
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Sales")
+plt.ylabel("Predicted Sales")
+plt.title("Actual vs Predicted Sales")
+plt.show()
+
+print("Sales Prediction Completed Successfully!")
